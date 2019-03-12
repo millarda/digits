a754bb44b2c49feaf06b1688fbb5361466925b6d
Andrew Millard <akm.scout@gmail.com>
2019-03-11 22:25:37 -1000

    landing page mockup finished in 11 minutes

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,22 +1,41 @@
 import React from 'react';
-import { Grid, Image } from 'semantic-ui-react';
+import { Grid, Icon, Header } from 'semantic-ui-react';
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
   render() {
     return (
-        <Grid verticalAlign='middle' textAlign='center' container>
+        <div className='background_image'>
+          <Grid verticalAlign='middle' textAlign='center' columns={3} container inverted>
 
-          <Grid.Column width={4}>
-            <Image size='small' circular src="/images/meteor-logo.png"/>
-          </Grid.Column>
+            <Grid.Column width={4} textAlign='center'>
+              <Icon name="microphone" size='huge'/>
+              <Header as="h1" inverted>Microphones</Header>
+              <Header as="h2" inverted>
+                  microphones sure are something. they let you record your voice and also clean your phones all in one.
+              </Header>
+            </Grid.Column>
 
-          <Grid.Column width={8}>
-            <h1>Welcome to this template</h1>
-            <p>Now get to work and modify this app!</p>
-          </Grid.Column>
+            <Grid.Column width={4} textAlign='center'>
+              <Icon name="globe" size='huge'/>
+              <Header as="h1" inverted>The World</Header>
+              <Header as="h2" inverted>
+                The world sure is important, it has you and me in it.
+                It also has water and plants and boy o boy are those things cool.
+              </Header>
+            </Grid.Column>
 
-        </Grid>
+            <Grid.Column width={4} textAlign='center'>
+              <Icon name="bell outline" size='huge'/>
+              <Header as="h1" inverted>Bells</Header>
+              <Header as="h2" inverted>
+                Bells are pretty cool they make noise. You could record that
+                noise with a microphone. crazy how objects can interact like that.
+              </Header>
+            </Grid.Column>
+
+          </Grid>
+        </div>
     );
   }
 }
