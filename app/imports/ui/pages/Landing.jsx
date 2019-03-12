1cdf35e8b66a64afc8e6e6dbf8be86efed333613
Andrew Millard <akm.scout@gmail.com>
2019-03-11 22:12:24 -1000

    landing page mockup finished in 28 minutes.

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,22 +1,34 @@
 import React from 'react';
-import { Grid, Image } from 'semantic-ui-react';
+import { Grid, Icon, Header} from 'semantic-ui-react';
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
   render() {
     return (
-        <Grid verticalAlign='middle' textAlign='center' container>
+        <div className="background_image">
+          <Grid verticalAlign='middle' textAlign='center' container>
 
-          <Grid.Column width={4}>
-            <Image size='small' circular src="/images/meteor-logo.png"/>
-          </Grid.Column>
+            <Grid.Column width={5} textAlign='center'>
+              <Icon name="home" size="huge"/>
+              <Header as="h1" inverted>House</Header>
+              <Header as="h2" color='white' inverted>woohoo houses ya i like houses otherwise
+                i would get all wet when it rained unless I had an umbrella</Header>
+            </Grid.Column>
 
-          <Grid.Column width={8}>
-            <h1>Welcome to this template</h1>
-            <p>Now get to work and modify this app!</p>
-          </Grid.Column>
+            <Grid.Column width={5} textAlign='center'>
+              <Icon name="blind" size="huge"/>
+              <Header as="h1" inverted>Old People</Header>
+              <Header as="h2" inverted>as time goes by people get old that sure is something maybe one day old people will not be old who knows</Header>
+            </Grid.Column>
 
-        </Grid>
+            <Grid.Column width={5} textAlign='center' inverted>
+              <Icon name="question circle" size="huge"/>
+              <Header white as="h1" color='white' inverted>Questions</Header>
+              <Header as="h2" color='white' inverted>Questions are for figuring things out. I should ask more questions since I have nothing figured out woweee</Header>
+            </Grid.Column>
+
+          </Grid>
+        </div>
     );
   }
 }
