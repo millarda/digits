9aa0417b41c821e05bd50f19c5a1f0023e7909fe
Andrew Millard <akm.scout@gmail.com>
2019-03-25 23:32:08 -1000

    notes-2 finished in 66 minutes.
    Bulk was finished in 25 minutes but took a long time to find an incorrect import statement. so sad

--- app/imports/startup/server/note.js
+++ app/imports/startup/server/note.js
@@ -1,11 +1,11 @@
 import { Meteor } from 'meteor/meteor';
-import { Contacts } from '../../api/contact/contact.js';
+import { Notes } from '../../api/note/note.js';
 
 /** This subscription publishes only the documents associated with the logged in user */
 Meteor.publish('Notes', function publish() {
   if (this.userId) {
     const username = Meteor.users.findOne(this.userId).username;
-    return Contacts.find({ owner: username });
+    return Notes.find({ owner: username });
   }
   return this.ready();
 });
